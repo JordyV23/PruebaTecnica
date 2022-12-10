a6a77de631f13cd9593cb3e626a30587f8ea3fb2
JordyV23 <jordyvargas0123@gmail.com>
2022-12-09 20:59:36 -0600

    endpoints de crear cliente y crear servicios, list

--- server/models/db.js
+++ server/models/db.js
@@ -12,6 +12,47 @@ const getServices = async function () {
     }
 }
 
+const getClients = async function () {
+    try {
+        let pool = await sql.connect(config);
+        let data = await pool.request().execute("spGetClients");
+        return data.recordsets;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+const insertClient = async function (correo,telefono) {
+    try {
+        let pool = await sql.connect(config);
+        let data = await pool.request()
+        .input("correo",sql.NVarChar,correo)
+        .input("telefono",sql.NVarChar,telefono)
+        .execute("SpCreateClient");
+        return data.recordsets;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const insertPaymentServices = async function (idCobro,idServicio,volumen) {
+    try {
+        let pool = await sql.connect(config);
+        let data = await pool.request()
+        .input("idcobro",sql.Int,idCobro)
+        .input("idServicio",sql.Int,idServicio)
+        .input("volumen",sql.Decimal,volumen)
+        .execute("SpCreatePaymentServices");
+        return data.recordsets;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 module.exports = {
-    getServices:getServices
+    getServices:getServices,
+    getClients:getClients,
+    insertClient:insertClient,
+    insertPaymentServices:insertPaymentServices
 }
--- server/server.js
+++ server/server.js
@@ -2,17 +2,26 @@ const express = require('express');
 const app = express();
 const db = require('./models/db')
 
-app.get("/", (req,res) => {
-    res.redirect("/services")
-})
-
 app.get("/services", (req,res) => {
     db.getServices().then((data) => {
-        //res.send(data[0])
         res.json(data[0])
     })
 })
 
+app.get("/listClients", (req,res) => {
+    db.getClients().then((data) => {
+        res.json(data[0])
+    })
+})
+
+app.post("/createClient", (req,res) => {
+   db.insertClient(req.body.correo, req.body.telefono);
+   res.redirect('listClients');
+})
+
+app.post("/createServices", (req,res) => {
+    db.insertPaymentServices(req.body.cobro,req.body.servicio,req.body.volumen);
+})
 
 
 app.listen('2323', () => {console.log("servidor en: http://localhost:2323")})
